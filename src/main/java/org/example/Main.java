987e088fcdac3d7558109b196448bf5350605b1e
Nolegal <pike1988@i.ua>
2023-07-03 11:12:06 +0300

    2ee

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-/*
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -7,4 +7,3 @@ public class Main {
 }
 
 
- */
