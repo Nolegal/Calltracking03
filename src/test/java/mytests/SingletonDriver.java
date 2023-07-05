09d77091ca853fda1d31058c191d1585a85c9be7
Nolegal <pike1988@i.ua>
2023-07-05 14:07:43 +0300

    22 commit

--- src/test/java/mytests/SingletonDriver.java
+++ src/test/java/mytests/SingletonDriver.java
@@ -12,7 +12,7 @@ public class SingletonDriver {
     public static WebDriver getSingletonInstance() {
         if (null == driver) {
         //   System.setProperty("webdriver.chrome.driver", ".//driver/chromedriver");
-         //     driver = new ChromeDriver();
+         //    driver = new ChromeDriver();
 
             System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
